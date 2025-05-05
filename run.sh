d1fdacf4c11e1a43a6d45a813c1b59fe5d6b40d9
rafatspn <rafatspn@gmail.com>
2025-05-05 05:13:00 +0000

    Multiple seed agent version added

--- run.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-python3 /home/rafatspn/AATune/utils/get_best_subset_of_params.py -csv=performance_data/all_results_intel.csv -executable=3mm_kernel_p1 -o=best_3mm_kernel_p1_intel.json
-
-python3 /home/rafatspn/AATune/seed/sd_pkr_agent.py -ir=/home/rafatspn/AATune/llvm-ir/3mm_kernel_p1.ll 
